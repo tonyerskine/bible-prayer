1f419e5903516a68482f69f3fa1d54005e6f4af5
Tony Erskine <tony@sharptop.co>
2019-02-14 11:52:18 -0500

    got pronouns working

--- ephesians-3-14-19/ephesians-3-14-19.js
+++ ephesians-3-14-19/ephesians-3-14-19.js
@@ -1,28 +1,35 @@
 $(document).ready(function () {
     console.log('document ready')
-    showHidePlurals();
-    $('input[type=radio]').change(showHidePlurals)
+    handlePlurality();
     $('input[type=text]').keyup(fillInValues)
     $(`#version`).change(changeVersion)
+    $(`#pronoun`).change(fillPronouns)
 })
 
 function fillInValues() {
     let who = $(`#who`).val()
     console.log(`filling in values for '${who}'`)
     $(`.who`).html(who)
-    $(`.possessive`).append(`'s`)
 }
 
-function showHidePlurals() {
-    let plural = $('input[name=plural]:checked').val()
-    if (plural === `false`) {
-        $("span.plural").hide()
-        $("span.singular").show()
-    }
-    else {
-        console.log('showing plurals')
-        $("span.plural").show()
-        $("span.singular").hide()
+function handlePlurality() {
+    handelPlurity('noun');
+    handelPlurity('verb');
+
+    function handelPlurity(partOfSpeech) {
+        let pronoun = $(`#pronoun`).val()
+        let singular = pronouns[pronoun].singular[partOfSpeech]
+        console.log(`singluar[${partOfSpeech}] === `, singular)
+        if (singular) {
+            console.log(`showing singular ${partOfSpeech}`)
+            $(`span.plural.${partOfSpeech}`).hide()
+            $(`span.singular.${partOfSpeech}`).show()
+        }
+        else {
+            console.log(`showing plurals ${partOfSpeech}`)
+            $(`span.plural.${partOfSpeech}`).show()
+            $(`span.singular.${partOfSpeech}`).hide()
+        }
     }
 }
 
--- ephesians-3-14-19/pronouns.js
+++ ephesians-3-14-19/pronouns.js
@@ -3,36 +3,71 @@ var pronouns = {
         subject: 'I',
         object: 'me',
         posessive: 'my',
-        reflexive: 'myself'
+        reflexive: 'myself',
+        singular: {
+            noun: true,
+            verb: false,
+        },
+
     },
     you: {
         subject: 'you',
         object: 'you',
         posessive: 'your',
-        reflexive: 'yourself'
+        reflexive: 'yourself',
+        singular: {
+            noun: true,
+            verb: false,
+        },
     },
     he: {
         subject: 'he',
         object: 'him',
         posessive: 'his',
-        reflexive: 'himself'
+        reflexive: 'himself',
+        singular: {
+            noun: true,
+            verb: true,
+        },
     },
     she: {
         subject: 'she',
         object: 'her',
-        posessive: 'hers',
-        reflexive: 'herself'
+        posessive: 'her',
+        reflexive: 'herself',
+        singular: {
+            noun: true,
+            verb: true,
+        },
     },
     we: {
         subject: 'we',
         object: 'us',
         posessive: 'our',
-        reflexive: 'ourselves'
+        reflexive: 'ourselves',
+        singular: {
+            noun: false,
+            verb: false,
+        },
     },
     they: {
         subject: 'they',
         object: 'them',
         posessive: 'their',
-        reflexive: 'themselves'
+        reflexive: 'themselves',
+        singular: {
+            noun: false,
+            verb: false,
+        },
     },
+}
+
+function fillPronouns() {
+    let pronoun = $(`#pronoun`).val()
+    console.log(`filling pronouns with '${pronoun}'`)
+    $(`.pronoun.subject`).html(pronouns[pronoun].subject)
+    $(`.pronoun.object`).html(pronouns[pronoun].object)
+    $(`.pronoun.possessive`).html(pronouns[pronoun].posessive)
+    $(`.pronoun.reflexive`).html(pronouns[pronoun].reflexive)
+    handlePlurality()
 }
